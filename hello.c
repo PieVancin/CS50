436fb3cac0e365410e6a6ccdd9ab710a53daadd4
PieVancin <PieVancin@users.noreply.github.com>
2020-07-15 12:00:48 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
+    //asks for the person's name
     string name = get_string("What's your name?\n");
-
+    
+    //concatenate the name that was given previously with a greeting
     printf("hello, %s\n", name);
 }
