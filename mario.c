d5e286e03f0adfb75ac1a4f59525ea4cb55aa203
PieVancin <PieVancin@users.noreply.github.com>
2020-07-24 15:24:23 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
-
- {  int i;
-  do 
-
-    //concatenate the name that was given previously with a greeting
-   for (int i = 0; i = <10; i++);
- {
-    printf (%i\n); }
- }
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
