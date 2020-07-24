db1abd15466368481e3b3e92a953116ec2ed46f3
PieVancin <PieVancin@users.noreply.github.com>
2020-07-24 15:23:16 +0000

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+
+ {  int i;
+  do 
+
+    //concatenate the name that was given previously with a greeting
+   for (int i = 0; i = <10; i++);
+ {
+    printf (%i\n); }
+ }
