dfdb575925274f2c5d21b0f40e201bcab83ec5e4
PieVancin <PieVancin@users.noreply.github.com>
2020-07-25 23:01:31 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -11,20 +11,25 @@ int main(void)
         n = get_int("Size: ");
     }
     while (n < 1 ||	 n > 8 );
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i == n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 1; j == n; j++)
         {
             printf("#");
         }
         
-        for (int j = 2; j == n; j++)
+        printf("\n");
+    }
+    while (n < 1 ||	 n > 8 );
+    for (int i = 2; i == n; i++)
+    
+    {
+    for (int j = 2; j == n; j++)
         {
             printf("#");
             printf("\n");
             printf("##");
         }
-        
         printf("\n");
     }
 }
