4794ef87cd2800f2c6e376d4df62db89e1fcd550
PieVancin <PieVancin@users.noreply.github.com>
2020-08-03 12:55:11 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -19,23 +19,15 @@ int main(void)
     // If it's 1
     for (int i = 1; i == n; i++)
     {
-        for (int j = 1; j == n; j++)
-        {
-            printf("#  #");
-        }
-        
-        printf("\n");
+       printf("#  #");
+       printf("\n");
     }
     
     // If it's 2
     for (int i = 2; i == n; i++)
     
     {
-        for (int j = 2; j == n; j++)
-        {
-            printf(" #  #\n##  ##");
-            
-        }
+        printf(" #  #\n##  ##");
         printf("\n");
     }
     
@@ -43,80 +35,49 @@ int main(void)
     for (int i = 3; i == n; i++)
     
     {
-        for (int j = 3; j == n; j++)
-        {
-            printf("  #  #\n ##  ##\n###  ###");
-            
-        }
-        printf("\n");
+       printf("  #  #\n ##  ##\n###  ###");
+       printf("\n");
     }
     
     // If it's 4
     for (int i = 4; i == n; i++)
     
     {
-        for (int j = 4; j == n; j++)
-        {
-            printf("   #  #\n  ##  ##\n ###  ###\n####  ####");
-            
-        }
-        printf("\n");
+       printf("   #  #\n  ##  ##\n ###  ###\n####  ####");
+       printf("\n");
     }
     
     // If it's 5
     for (int i = 5; i == n; i++)
     
     {
-        for (int j = 5; j == n; j++)
-        {
-            printf("    #  #\n   ##  ##\n  ###  ###\n ####  ####\n#####  #####");
-            
-        }
-        printf("\n");
-        
-        
+       printf("    #  #\n   ##  ##\n  ###  ###\n ####  ####\n#####  #####");
+       printf("\n");
     }
     
     // If it's 6
     for (int i = 6; i == n; i++)
     
     {
-        for (int j = 6; j == n; j++)
-        {
-            printf("     #  #\n    ##  ##\n   ###  ###\n  ####  ####\n #####  #####\n######  ######");
-            
-        }
-        printf("\n");
-        
-        
+       printf("     #  #\n    ##  ##\n   ###  ###\n  ####  ####\n #####  #####\n######  ######");
+       printf("\n");
     }
     
     // If it's 7
     for (int i = 7; i == n; i++)
     
     {
-        for (int j = 7; j == n; j++)
-        {
-            printf("      #  #\n     ##  ##\n    ###  ###\n   ####  ####\n  #####  #####\n ######  ######\n#######  #######");
-            
-        }
-        printf("\n");
-        
-        
+       printf("      #  #\n     ##  ##\n    ###  ###\n   ####  ####\n  #####  #####\n ######  ######\n#######  #######");
+       printf("\n");
     }
     
     // If it's 8
     for (int i = 8; i == n; i++)
     
+    
     {
-        for (int j = 8; j == n; j++)
-        {
-            printf("       #  #\n      ##  ##\n     ###  ###\n    ####  ####\n   #####  #####\n  ######  ######\n #######  #######\n########  ########");
-            
-        }
-        printf("\n");
-        
-        
+       printf("       #  #\n      ##  ##\n     ###  ###\n    ####  ####\n   #####  #####\n  ######  ######\n #######  #######\n########  ########");
+       printf("\n");
     }
     
     
