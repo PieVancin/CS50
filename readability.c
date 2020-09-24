7e8b4700740d4ae415fdaee8b59b75441db90426
PieVancin <PieVancin@users.noreply.github.com>
2020-09-24 12:30:35 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -11,37 +11,37 @@ int main(void)
 
     float letters = 0, words = 1, sentences = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
         {
-            if (isalpha(text[i]))
-            {
-                letters++;
-            }
-            if (isspace(text[i]))
-            {
-                words++;
-            }
-            if (text[i] == '.' || text[i] == '!' || text[i] == '?')
-            {
-            sentences++;
-            }
+            letters++;
         }
+        if (isspace(text[i]))
+        {
+            words++;
+        }
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
 
     float l = letters / words * 100;
     float s = sentences / words * 100;
     float index = 0.0588 * l - 0.296 * s - 15.8;
-    int indexi = round(index);
+    int indexx = round(index);
 
-    if (indexi >= 1 && indexi <=16)
-        {
-            printf("Grade %i\n", indexi);
-        }
+    if (indexx >= 1 && indexx <= 16)
+    {
+        printf("Grade %i\n", indexx);
+    }
     else
     {
-        if (indexi < 1)
+        if (indexx < 1)
         {
             printf("Before Grade 1\n");
         }
-        if (indexi > 16)
+        if (indexx > 16)
         {
             printf("Grade 16+\n");
         }
