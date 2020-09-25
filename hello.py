a5a10ab4db9d714c02552f24fa8f689160fb009e
PieVancin <PieVancin@users.noreply.github.com>
2020-09-25 12:46:34 +0000

    automated commit by submit50

---
+++ hello.py
@@ -0,0 +1,7 @@
+from cs50 import get_string
+
+
+#asks for the person's name
+name = get_string("What's your name?\n");
+#concatenate the name that was given previously with a greeting
+print("hello,", name);
