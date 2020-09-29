4d18fbbf97deab0a424f41acd4181252f6a179a3
PieVancin <PieVancin@users.noreply.github.com>
2020-09-29 03:20:43 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
-    string s = 0;
-    if ((argc == 2) && (atoi(argv[1]) >=1 && atoi(argv[1]) <= 5000))
+    
+    string s = 0; 
+    if (argc == 2)
     {
     s = get_string("plaintext: ");
     printf("ciphertext:  ");
@@ -19,12 +21,20 @@ int main(int argc, string argv[])
     
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if (s[i] >= 'A' && s[i] <= 'z')
+        if (isupper(s[i]))
         {
-            printf("%c", s[i] + atoi(argv[1]));
+            printf("%c", s[i] + atoi(argv[1]) % 26);
             
         }
-        else
+        if (islower(s[i]))
+        {
+            printf("%c", s[i] + atoi(argv[1]) % 26);
+        }
+        if (isdigit(s[i]))
+        {
+            printf("%c", s[i]);
+        }
+        if (isspace(s[i]))
         {
             printf("%c", s[i]);
         }
