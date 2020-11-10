9cf4d64551d2bd7f9e633a9d597054d01f1a1a75
PieVancin <PieVancin@users.noreply.github.com>
2020-11-10 16:15:14 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
+typedef uint8_t BYTE;
+#define BLOCKSIZE 512
 int main(int argc, char *argv[])
 {
 
     if (argc != 2)
-    {
+    {   
+        printf("usage: ./recover image\n");
         return 1;
     }
 
-
-    FILE *file = fopen(argv[1], "r");
-    if (!file)
-    {
+    char* file_n=argv[1];
+    FILE *file_p = fopen(argv[1], "r");
+    if (!file_p)
+    {   printf("it can't be open");
         return 1;
     }
-
-    unsigned char bytes[3];
-    fread(bytes, 3, 1, file);
-
-    // Check first three bytes
-    if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff)
+    
+    
+    
+    unsigned char buffer[BLOCKSIZE];
+    int nimages = 0;
+    FILE *output_point=NULL;
+    int found_jpg=0;
+    
+    while (fread(&buffer, BLOCKSIZE, 1, file_p)==1)
     {
-        printf("Maybe\n");
-    }
-    else
-    {
-        printf("No\n");
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            if (found_jpg==1)
+            {
+                fclose(output_point);
+            }
+            else
+            {
+               found_jpg=1; 
+            }
+            
+            char name[8];
+            sprintf(name, "%03i.jpg", nimages);
+            output_point = fopen(name, "w");
+            nimages++;
+        }
+        if (found_jpg==1)
+        {
+            fwrite(&buffer, BLOCKSIZE, 1, output_point);
+        }
     }
 
-    // Close file
-    fclose(file);
+    
 }
