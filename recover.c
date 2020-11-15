ffeb15c2a5ed3eea1755c8a56633c581b811d746
PieVancin <PieVancin@users.noreply.github.com>
2020-11-15 01:11:24 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -4,10 +4,11 @@
 #include <stdbool.h>
 
 typedef uint8_t BYTE;
+
 #define BLOCKSIZE 512
 int main(int argc, char *argv[])
 {
-
+    //check if it's usage is correct
     if (argc != 2)
     {   
         printf("usage: ./recover image\n");
@@ -17,17 +18,16 @@ int main(int argc, char *argv[])
     char* file_n=argv[1];
     FILE *file_p = fopen(argv[1], "r");
     if (!file_p)
-    {   printf("it can't be open");
+    {  
+        printf("Cannot be open");
         return 1;
     }
     
-    
-    
     unsigned char buffer[BLOCKSIZE];
     int nimages = 0;
-    FILE *output_point=NULL;
     int found_jpg=0;
-    
+    FILE *output_point=NULL;
+
     while (fread(&buffer, BLOCKSIZE, 1, file_p)==1)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
             }
             else
             {
-               found_jpg=1; 
+                found_jpg = 1; 
             }
             
             char name[8];
@@ -46,11 +46,14 @@ int main(int argc, char *argv[])
             output_point = fopen(name, "w");
             nimages++;
         }
-        if (found_jpg==1)
+        if (found_jpg == 1)
         {
             fwrite(&buffer, BLOCKSIZE, 1, output_point);
         }
     }
+    
+    fclose(output_point);
+    fclose(file_p);
 
     
 }
