536445606426d69bc95d9ea22c078963ef388b92
PieVancin <PieVancin@users.noreply.github.com>
2020-12-08 01:55:50 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,34 +1,13 @@
 from cs50 import get_int
 
 
-    
 # Asks the height of the wall
-n = get_int("Height: ");
-i = n
+while True:
+ n = get_int("Height: ")
+ if n > 0 and n < 9:
+  break
 
-while(n>0 and n<9):
-    if i==1:
-     print("#")
-     break
-    elif i==2:
-     print(" # \n##")
-     break
-    elif i==3:
-     print("  #\n ##\n###")
-     break
-    elif i==4:
-     print("   #\n  ##\n ###\n####")
-     break
-    elif i==5:
-     print("    #\n   ##\n  ###\n ####\n#####")
-     break
-    elif i==6:
-     print("     #\n    ##\n   ###\n  ####\n #####\n######")
-     break
-    elif i==7:
-     print("      #\n     ##\n    ###\n   ####\n  #####\n ######\n#######")
-     break
-    elif i==8:
-     print("       #\n      ##\n     ###\n    ####\n   #####\n  ######\n #######\n########")
-     break
-    
+#loop
+for i in range(n):
+ print((n - 1 - i) * " ", end="")
+ print((i + 1) * "#")
