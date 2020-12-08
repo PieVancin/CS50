df44741e62ce20541de0021598fff9926a46cc16
PieVancin <PieVancin@users.noreply.github.com>
2020-12-08 19:16:06 +0000

    automated commit by submit50

--- cash.py
+++ cash.py
@@ -9,21 +9,21 @@ cent = dollar * 100
 coins = 0
 
 # ask again if the input is less then 0
-if dollar <0:
+if dollar < 0:
     dollar = get_float("Change owed:")
 
 # adds the coins for the respective change     
 while cent >= 25:
-    coins+=1
+    coins += 1
     cent = cent - 25
 while cent >= 10:
-    coins+=1
+    coins += 1
     cent = cent - 10
 while cent >= 5:
-    coins+=1
+    coins += 1
     cent = cent - 5
 while cent >= 1:
-    coins+=1
+    coins += 1
     cent = cent - 1
     
 # print the result
