0041929c1c441e7fdb5d138a919bf8e72db3a6bd
PieVancin <PieVancin@users.noreply.github.com>
2020-12-08 19:14:16 +0000

    automated commit by submit50

--- cash.py
+++ cash.py
@@ -2,21 +2,29 @@ from cs50 import get_float
 
 dollar = float
 cent = int
-while(True):
+
+# prompts user to input the amount owed
+dollar = get_float("Change owed:")
+cent = dollar * 100
+coins = 0
+
+# ask again if the input is less then 0
+if dollar <0:
     dollar = get_float("Change owed:")
-    cent = round(dollar * 100)
-while (dollar < 0.00):
-    coins = 0
-while (cent >= 25):
-    coins+1
+
+# adds the coins for the respective change     
+while cent >= 25:
+    coins+=1
     cent = cent - 25
-while (cent >= 10 and cent < 25):
-    coins+1
+while cent >= 10:
+    coins+=1
     cent = cent - 10
-while (cent >= 5 and cent < 10):
-    coins+1
+while cent >= 5:
+    coins+=1
     cent = cent - 5
-while (cent >= 1 and cent < 5):
-    coins+1
+while cent >= 1:
+    coins+=1
     cent = cent - 1
+    
+# print the result
 print(coins, "coins in return")
